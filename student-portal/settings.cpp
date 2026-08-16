145ea1a8702aa6f700e93b32a443e6dd53cb3603
chandunukala <chandu.nukala9@gmail.com>
2026-08-16 09:43:30 +0530

    Add dark mode preference management

--- student-portal/settings.cpp
+++ student-portal/settings.cpp
@@ -13,6 +13,11 @@ void setNotifications(StudentSettings& settings, bool enabled)
     settings.notificationsEnabled = enabled;
 }
 
+void setDarkMode(StudentSettings& settings, bool enabled)
+{
+    settings.darkModeEnabled = enabled;
+}
+
 void displaySettings(const StudentSettings& settings)
 {
     std::cout << "\n--- Student Settings ---\n";
@@ -40,6 +45,11 @@ int main()
     setNotifications(settings, false);
 
 std::cout << "\nAfter notification preference change:\n";
+displaySettings(settings);
+
+setDarkMode(settings, true);
+
+std::cout << "\nAfter dark mode preference change:\n";
 displaySettings(settings);
 
     return 0;
